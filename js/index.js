8b679f65e22fd2642099f7ea12fb0dc900fa6a61
Joaquin Orozco <joaquinalexisorozco@gmail.com>
2022-02-17 21:34:45 -0300

    primer commit portafolio

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let sectionInicio = document.querySelector("#section-inicio");
+let sectionSobreMi = document.querySelector("#section-sobre-mi");
+let sectionSkills = document.querySelector("#section-skills");
+
+ScrollReveal().reveal(sectionInicio, {duration: 2000});
+ScrollReveal().reveal(sectionSobreMi, {duration: 2000});
+ScrollReveal().reveal(sectionSkills, {duration: 2000});
