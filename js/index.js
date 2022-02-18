6ada2c608164f2c5397a5943bfc7df5f491cce19
Joaquin Orozco <joaquinalexisorozco@gmail.com>
2022-02-18 01:54:03 -0300

    v2.0

--- js/index.js
+++ js/index.js
@@ -2,6 +2,16 @@ let sectionInicio = document.querySelector("#section-inicio");
 let sectionSobreMi = document.querySelector("#section-sobre-mi");
 let sectionSkills = document.querySelector("#section-skills");
 
-ScrollReveal().reveal(sectionInicio, {duration: 2000});
-ScrollReveal().reveal(sectionSobreMi, {duration: 2000});
-ScrollReveal().reveal(sectionSkills, {duration: 2000});
+ScrollReveal().reveal(sectionInicio, {
+    duration: 1000,
+    scale: 0.5,
+});
+ScrollReveal().reveal(sectionSobreMi, {
+    duration: 1000,
+    scale: 0.5
+});
+
+ScrollReveal().reveal(sectionSkills,{
+    duration: 1000,
+    scale: 0.5
+});
