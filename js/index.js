1079cba86686e9a9cf95ef10055cb0c7e32f2b6e
Joaquin Orozco <joaquinalexisorozco@gmail.com>
2022-03-05 15:40:52 -0300

    Actualizacion

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 const templateHeader = `
-<nav class="navbar navbar-expand-lg navbar-light">
+<nav class="navbar navbar-expand-lg bg-dark">
     <div class="container">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse float-end" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="nav-items">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Sobre mi</a>
+                <a class="nav-link active text-light" aria-current="page" href="#">Sobre mi</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Contacto</a>
+                <a class="nav-link active text-light" aria-current="page" href="#">Contacto</a>
             </li>
             </ul>
         </div>
@@ -23,13 +23,14 @@ const header = document.querySelector("#header");
 const templateAddHeader = header.innerHTML = templateHeader;
 
 // ---------------------ANIMATION------------------------------
-            
+
+
 let sectionInicio = document.querySelector("#section-inicio");
 let sectionSobreMi = document.querySelector("#section-sobre-mi");
 let sectionSkills = document.querySelector("#section-skills");
 
 ScrollReveal().reveal(sectionInicio, {
-    duration: 1000,
+    duration: 500,
     scale: 0.5,
     rotate: {
         x:-180,
@@ -45,4 +46,22 @@ ScrollReveal().reveal(sectionSobreMi, {
 ScrollReveal().reveal(sectionSkills,{
     duration: 1000,
     scale: 0.5
-});
+})
+
+const maquina = document.querySelector('.maquina-escribir');
+console.log(maquina);
+
+const maquinaDeEscribir = (text = '', tiempo = 200, etiqueta = '') => {
+    let arrayLetter = text.split('');
+    console.log(arrayLetter);
+    etiqueta.innerHTML = '';
+    let cont = 0;
+    let escribir = setInterval(function() {
+        etiqueta.innerHTML += arrayLetter[cont];
+        cont++;
+        if (cont === arrayLetter.length) {
+            clearInterval(escribir);
+        }
+    },tiempo)
+}
+maquinaDeEscribir("Joaquin Orozco", 300, maquina)
